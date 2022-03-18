5b9e39d3cfb46243f8e7b6dcea99aa0f09761b96
ckwogus37 <ckwogus37@gmail.com>
2022-03-18 10:01:48 +0900

    prototype site

---
+++ server.js
@@ -0,0 +1,18 @@
+//node_modules에 있는 express 관련 파일을 가져온다.
+var express = require('express')
+
+//express는 함수이므로, 반환값을 변수에 저장한다.
+var app = express()
+
+// request 와 response 라는 인자를 줘서 콜백 함수를 만든다.
+// localhost:3000 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
+//__dirname은 요청하고자 하는 파일의 경로를 단축시켜주는 절대경로 식별자이다.
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/MapleStoryNewbie/index.html")
+})
+
+//3000 포트로 서버 오픈
+app.listen(3000, function(){console.log('Start! express server on port 3000');
+});
+
+//터미널에서 node server.js 명령어를 통해 node express 서버를 가동시킬 수 있다.
